5f45d5896633fb250d5bbaf388b214a0ccdd8cc5
shabaaz27 <khanshabaaz05@gmail.com>
2023-03-08 13:08:31 +0530

    Basic TDD Test

---
+++ src/components/greet/greet.test.tsx
@@ -0,0 +1,25 @@
+
+
+import { render,screen } from "@testing-library/react"
+import { Greet } from "./greet"
+
+
+
+/**
+ * Greet should render the text hello and if a name is passed into the component
+ * it should render hello followed by the name
+ */
+
+
+
+test('Greet Renders correctly',()=>{
+    render(<Greet/>)
+    const textElement = screen.getByText('Hello')
+    expect(textElement).toBeInTheDocument()
+})
+
+test('Greet render with a name',()=>{
+    render(<Greet name="shabaaz"/>)
+    const textElement =screen.getByText('Hello shabaaz')
+    expect(textElement).toBeInTheDocument()
+})
