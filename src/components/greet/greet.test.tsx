a0a66693210129f2bba3aec2818db9c196d3d369
shabaaz27 <khanshabaaz05@gmail.com>
2023-03-08 14:54:22 +0530

    Grouping Test using describe

--- src/components/greet/greet.test.tsx
+++ src/components/greet/greet.test.tsx
@@ -1,19 +1,21 @@
-import { render,screen } from "@testing-library/react"
-import { Greet } from "./greet"
+import { render, screen } from "@testing-library/react";
+import { Greet } from "./greet";
 
 /**
  * Greet should render the text hello and if a name is passed into the component
  * it should render hello followed by the name
  */
 
-test('Greet Renders correctly',()=>{
-    render(<Greet/>)
-    const textElement = screen.getByText('Hello')
-    expect(textElement).toBeInTheDocument()
-})
+describe("Greet", () => {
+  test(" Renders correctly", () => {
+    render(<Greet />);
+    const textElement = screen.getByText("Hello");
+    expect(textElement).toBeInTheDocument();
+  });
 
-test.skip('Greet render with a name',()=>{
-    render(<Greet name="shabaaz"/>)
-    const textElement =screen.getByText('Hello shabaaz')
-    expect(textElement).toBeInTheDocument()
-})
+  test.skip("render with a name", () => {
+    render(<Greet name="shabaaz" />);
+    const textElement = screen.getByText("Hello shabaaz");
+    expect(textElement).toBeInTheDocument();
+  });
+});
