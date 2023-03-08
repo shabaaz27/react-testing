9eca3d81a757b3abb53155afdb1889fd476faf5b
shabaaz27 <khanshabaaz05@gmail.com>
2023-03-08 14:35:53 +0530

    .

--- src/components/greet/greet.test.tsx
+++ src/components/greet/greet.test.tsx
@@ -1,16 +1,10 @@
-
-
 import { render,screen } from "@testing-library/react"
 import { Greet } from "./greet"
-
-
 
 /**
  * Greet should render the text hello and if a name is passed into the component
  * it should render hello followed by the name
  */
-
-
 
 test('Greet Renders correctly',()=>{
     render(<Greet/>)
